c51124c9ac180c09923ed1b5d48549397a214697
Sharad Chauhan <sharadsingh6464@gmail.com>
2024-02-15 23:33:05 +0530

    Resolve cors

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors(
   {
-    origin:["http://localhost:5173/"],
+    origin:["http://localhost:5173"],
     methods:["POST","GET"],
     credentials:true,
   }
