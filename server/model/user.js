f7867de540e9e0fb70c0518a1e28adcaffd4a18c
Sharad Chauhan <sharadsingh6464@gmail.com>
2024-02-15 22:30:14 +0530

    Connected Database to Mongo Atlas

--- server/model/user.js
+++ server/model/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const cart=require('./cart');
+require('dotenv').config({path:'./.env'});
+
+
 
 main()
 .then((res)=>{console.log("Connection is up")})
 .catch(err => console.log(err));
-
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/coffee');
+  await mongoose.connect(process.env.MONGO_URL);
 }
 
 // User Database Schema
