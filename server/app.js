f2fe163bd0a3ac309b2ebf211de2c973da466a36
Sharad Chauhan <sharadsingh6464@gmail.com>
2024-02-17 23:52:59 +0530

    Added Port

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors(
   {
-    origin:["http://localhost:5173"],
+    origin:["https://damium-coffee.netlify.app"],
     methods:["POST","GET"],
     credentials:true,
   }
