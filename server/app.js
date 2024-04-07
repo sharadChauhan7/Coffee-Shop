9b5d3621af6da8dfcfce78da74f22e541fdfc88b
Sharad Chauhan <sharadsingh6464@gmail.com>
2024-04-07 22:57:52 +0530

    Verced Deploy(Front-End)

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors(
   {
-    origin:["http://localhost:5173"],
+    origin:["https://mycoffeebeans.vercel.app"],
     methods:["POST","GET"],
     credentials:true,
   }
